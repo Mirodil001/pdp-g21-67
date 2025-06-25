3a66c8fe6f916700aba50f9f8c7868cc66f40b1d
Mirodil <ikromovmirodil008@gmail.com>
2025-06-25 16:49:30 +0500

    midle codes

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { moviesarray } from "./constants";
+import { allmovies } from "./elements";
 
 document.addEventListener("DOMContentLoaded", function () {
   // Genre selection animation
@@ -6,10 +7,54 @@ document.addEventListener("DOMContentLoaded", function () {
     "#genreList .list-group-item"
   ) as NodeListOf<HTMLLIElement>;
   genreItems.forEach((item) => {
+    moviesarray.forEach((movie) => {
+      const allmovies = movie;
+      for(let i = 0; i < moviesarray.length; i++){
+          document.createElement("tr");
+          
+      }
+    })
     item.addEventListener("click", function () {
       genreItems.forEach((i) => i.classList.remove("active"));
       this.classList.add("active");
-       
+
+      let movieshistory: any = [];
+
+      moviesarray.forEach((box) => {
+        if (item.innerText === "All") {
+          let generade = box.genre._id;
+          movieshistory.push(box);
+          console.log(generade);
+        }
+
+        if (item.innerText === "Comedy" && box.genre.name === "Comedy") {
+          let generade = box.genre._id;
+          movieshistory.push(box);
+          console.log(generade);
+        }
+        if (item.innerText === "Action" && box.genre.name === "Action") {
+          let generade = box.genre._id;
+          movieshistory.push(box);
+          console.log(generade);
+        }
+        if (item.innerText === "Romance" && box.genre.name === "Romance") {
+          let generade = box.genre._id;
+          movieshistory.push(box);
+          console.log(generade);
+        }
+        if (item.innerText === "Thriller" && box.genre.name === "Thriller") {
+          let generade = box.genre._id;
+          movieshistory.push(box);
+          console.log(generade);
+        }
+      });
+
+      console.log(movieshistory);
+      movieshistory = [];
+      // moviesarray.forEach((box) => {
+      //   let generade = box.genre.name;
+      //   c;
+      // });
     });
   });
 
@@ -26,6 +71,4 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-
-//UI Function
-
+//UI Functiongit
