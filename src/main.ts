d3bacab3aafdeac734339000657d758657277a95
islom1212 <islomaktamov488>
2025-06-25 14:55:22 +0500

    midle cods

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,26 @@
-document.addEventListener("DOMContentLoaded", function () {
-        // Genre selection animation
-        const genreItems = document.querySelectorAll(
-          "#genreList .list-group-item"
-        );
-        genreItems.forEach((item) => {
-          item.addEventListener("click", function () {
-            genreItems.forEach((i) => i.classList.remove("active"));
-            this.classList.add("active");
-          });
-        });
+import { moviesarray } from "./constants";
 
-        // Pagination animation
-        const pageItems = document.querySelectorAll("#pagination .page-item");
-        pageItems.forEach((item) => {
-          item.addEventListener("click", function (e) {
-            e.preventDefault();
-            pageItems.forEach((i) => i.classList.remove("active"));
-            this.classList.add("active");
-          });
-        });
-      });
+document.addEventListener("DOMContentLoaded", function () {
+  // Genre selection animation
+  const genreItems = document.querySelectorAll(
+    "#genreList .list-group-item"
+  ) as NodeListOf<HTMLLIElement>;
+  genreItems.forEach((item) => {
+    item.addEventListener("click", function () {
+      genreItems.forEach((i) => i.classList.remove("active"));
+      this.classList.add("active");
+    });
+  });
+
+  // Pagination animation
+  const pageItems = document.querySelectorAll(
+    "#pagination .page-item"
+  ) as NodeListOf<HTMLLIElement>;
+  pageItems.forEach((item) => {
+    item.addEventListener("click", function (e) {
+      e.preventDefault();
+      pageItems.forEach((i) => i.classList.remove("active"));
+      this.classList.add("active");
+    });
+  });
+});
