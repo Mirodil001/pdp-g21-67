310fe4c41c7142a69097f671c25a715908aa9b8f
islom1212 <islomaktamov488>
2025-06-25 16:53:56 +0500

    islom midle codes

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,28 @@
 import { moviesarray } from "./constants";
+import {
+  allmovies,
+  comedymovies,
+  actionmovies,
+  dramamovies,
+  thrillermovies,
+  category,
+} from "./elements";
+
+//UI FUNCTIONS
+
+function Category(length: number = 16) {
+  category.innerHTML = "";
+  for (let i = 1; i <= Math.ceil(length / 3); i++) {
+    let newLiElement = document.createElement("li");
+    newLiElement.classList.add("page-item");
+    let newAElement = document.createElement("a");
+    newAElement.classList.add("page-link");
+    newAElement.href = "#";
+    newAElement.textContent = `${i}`;
+    newLiElement.append(newAElement);
+    category.append(newLiElement);
+  }
+}
 
 document.addEventListener("DOMContentLoaded", function () {
   // Genre selection animation
@@ -24,3 +48,5 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+Math.ceil(moviesarray.length / 3);
